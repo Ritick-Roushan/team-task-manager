22a780db170259e9e554178109e5fa9469d56694
Ritick-Roushan <2k22.csaiml.2213607@gmail.com>
2026-05-01 05:00:12 +0530

    fixing the baseurl in frontend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://team-task-manager-production-0f5a.up.railway.app"
+  baseURL: "https://team-task-manager-production-0f5a.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
