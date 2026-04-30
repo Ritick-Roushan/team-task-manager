d8faec9b9c2c4c73f836e962140528755862099e
Ritick-Roushan <2k22.csaiml.2213607@gmail.com>
2026-05-01 03:42:27 +0530

    fixing the port issue

--- Backend/server.js
+++ Backend/server.js
@@ -22,4 +22,8 @@ app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes); // ✅ THIS WAS MISSING
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
