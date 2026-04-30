49f5a10c24e750ea54ed79dc6915d30adb590eee
Ritick-Roushan <2k22.csaiml.2213607@gmail.com>
2026-05-01 04:23:32 +0530

    fixing vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -17,8 +17,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-  host: true,
-  port: 8080,
-  allowedHosts: "all"
-}
+    host: true,
+    port: 8080,
+    allowedHosts: [
+      "resilient-nourishment-production-cac2.up.railway.app"
+    ]
+  }
 })
